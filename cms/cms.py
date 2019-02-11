21d0b74a31595acabe83920832de1e68b311314e
Björn Kristensson Alfsson <Bjorn.KristenssonAlfsson@alten.se>
2019-02-11 15:28:49 +0100

    Added eventlet requirement

--- cms/cms.py
+++ cms/cms.py
@@ -7,12 +7,14 @@ The whole point of Celery is to run it as a separate process
 so that your webapp could off-load computationally intense 
 tasks to it, so why are you trying to run Celery worker in the 
 same process as your webapp? 
+
+https://www.linode.com/docs/development/python/task-queue-celery-rabbitmq/
 """
 
 
 BASEDIR="/Users/bk930576/Python/downloads"
 
-app = Celery("CMS",
+app = Celery("cms",
              backend="rpc://",
              broker="pyamqp://guest@localhost//")
 
@@ -21,7 +23,7 @@ app = Celery("CMS",
 def download(url, filename):
     response = urllib.request.urlopen(url)
     data = response.read()
-    with open(BASEDIR+"/"+filename,"wb") as file:
+    with open(BASEDIR+"/"+filename, "wb") as file:
         file.write(data)
     file.close
 
